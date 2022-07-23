b2e3380e5430a6be9bfe0ef11dedad242fe1e5a9
dbhairavi0601 <dbhairavi0601@gmail.com>
2022-07-23 12:13:34 +0530

    sinew

---
+++ simpleint.py
@@ -0,0 +1,7 @@
+p=float(input("Enter the Principal Amount:"))
+n=float(input("Enter the number of years:"))
+r=float(input("Enter the Rate of interest:"))
+
+si= (p*n*r)/100
+
+print("The Simple interest is:",si)
